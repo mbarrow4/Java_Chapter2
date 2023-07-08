24dfe5c89be9cad5bdb19980e4beb0784b91077b
Monica Barrow <monicabarrow@gmail.com>
2023-07-08 08:28:38 -0400

    Add files via upload

---
+++ Escape_Characters.java
@@ -0,0 +1,24 @@
+//This program demonstrates the different escape characters
+
+public class Escape_Characters //Escape Character Class
+{
+
+	public static void main(String[] args) //Beginning of the Main Method
+	{
+		//New Line Escape Character
+		System.out.println("New \nLine");
+		System.out.println("This \nis \na \nLine");
+		
+		//Tab Escape Character
+		System.out.println("\t Tab Character");
+		
+		//Back Slash Character
+		System.out.println("\\");
+		
+		//Double Quotes
+		System.out.println("\"");
+	
+
+	}//End of Main Method
+
+}//End of Escape Character Class
