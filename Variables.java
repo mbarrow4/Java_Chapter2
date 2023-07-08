1ade417951a6f0054249caf60561cc0426d1be66
Monica Barrow <monicabarrow@gmail.com>
2023-07-08 08:42:38 -0400

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,26 @@
+/**Simple Program that shows the different data types in Java*/
+public class Variables //Class Declarations
+{
+	/** when a variable is OUTSIDE of main, "static" must be used*/
+	static int number = 0; 
+	
+	public static void main(String[] args) //main method begins execution of program
+	{
+		/**Variables must have a NAME and a TYPE*/
+		int a = 0;
+		double b = 0.25;
+		float c = 0.33f; //float variables must have a 'f' at the end
+		char d = 'a';
+		boolean f = false;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(f);
+		
+		
+		//integer is for whole numbers
+	} //end of main function
+
+}//end of class
