645cc026a84da02c615dd8fb3447884e975a6534
Monica Barrow <monicabarrow@gmail.com>
2023-07-08 08:26:03 -0400

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,21 @@
+/**This program illustrates the different comments available in Java */
+
+public class Comments //Class Declaration
+{
+	public static void main(String[] args) //main method begins execution of program
+	{
+	// This Is A Single Line Comment
+		
+	/**This is a Java Doc Comment*/
+		
+	/* This is a Block Comment
+	 * This is a Block Comment
+	 * This is a Block Comment
+	 * This is a Block Comment
+	 * This is a Block Comment
+	 */
+
+	
+	}//End main function
+
+}//end of COMMENTS class
