987537339454476f441de37d8837e466fdfae979
Monica Barrow <monicabarrow@gmail.com>
2023-07-08 08:26:48 -0400

    Add files via upload

---
+++ Concat.java
@@ -0,0 +1,12 @@
+/**This program obtains input from a user then concatenates the input to a string literal*/
+public class Concat //Class Declaration
+{
+
+	public static void main(String[] args) //main method begins execution of program
+	{
+		int age = 38;
+		System.out.println("My age is: " + age);//printf METHOD
+		
+	}//end of main
+
+}//end of class
