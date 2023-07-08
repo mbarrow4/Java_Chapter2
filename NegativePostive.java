fed5465733b1d3a815f8fa29aae9136a72c158da
Monica Barrow <monicabarrow@gmail.com>
2023-07-08 08:37:54 -0400

    Add files via upload

---
+++ NegativePostive.java
@@ -0,0 +1,56 @@
+/**Program that determines if a number is negative or positive*/
+public class NegativePostive 
+{
+
+	public static void main(String[] args) 
+	{
+	int number0=0;
+	int number1=1;
+	int number2=-2;
+	int number3=345690;
+	
+	if (number0>=1) 
+	{
+		System.out.println("Number is positive");
+	}
+	else if(number0==0) 
+	{
+		System.out.println("The number is equal to zero");
+	}
+	else 
+	{
+		System.out.println("The number is negative");
+	}
+	
+	
+
+	if (number1>=1) 
+	{
+		System.out.println("Number is positive");
+	}
+	else if(number1==0) 
+	{
+		System.out.println("The number is equal to zero");
+	}
+	else 
+	{
+		System.out.println("The number is negative");
+	}
+	
+	
+	
+	if (number2>=1) 
+	{
+		System.out.println("Number is positive");
+	}
+	else if(number2==0) 
+	{
+		System.out.println("The number is equal to zero");
+	}
+	else 
+	{
+		System.out.println("The number is negative");
+	}
+	}//End of Main
+
+}//End of Class
